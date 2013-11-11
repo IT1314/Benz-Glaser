f6717773c6ed1c4d91e0143d25dd77c8c95addde
IT1314 <christiancvc@gmx.de>
2013-11-11 16:55:02 +0100

    GatterTyp und Flipflop mit comment

---
+++ GatterTyp.h
@@ -0,0 +1,36 @@
+#ifndef GATTERTYP_H
+#define GATTERTYP_H
+
+#include <string>
+
+using namespace std;
+
+class GatterTyp{
+protected:
+
+	string name;
+	double grundLaufzeit;
+	short lastFaktor;
+	short lastKapazitaet;
+	short eingaenge;
+
+public:
+
+	GatterTyp();
+	 ~GatterTyp();
+	string getName(){ return name;}
+	double getGrundlaufzeit(){ return grundLaufzeit;}
+	short getLastFaktor(){return lastFaktor}
+	short getLastKapazitaet(){return lastKapazitaet;}
+	short getEingaege() {return eingaenge;}
+	bool getIsFlipflop(){return false ;}//?
+	void setName(string n){name = n;}
+	void setGrundLaufzeit(double gl){grundLaufzeit = gl;}
+	void setLastFaktor(short lf){lastFaktor = fl;}
+	void setLastKapazitaet(short lk){lastKapazitaet = lk;}
+	void setEingaenge(short ei){eingaenge = ei;}
+	
+	
+};
+
+#endif
