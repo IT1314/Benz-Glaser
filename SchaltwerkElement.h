eacf641b7210b212afbc31d17d75e0045045d5cb
finalcount <uenver_sefa@hotmail.de>
2013-11-15 18:43:02 +0100

    gitignore updated added schaltwerkelement

---
+++ SchaltwerkElement.h
@@ -0,0 +1,44 @@
+#include "GatterTyp.h"
+#include <string>
+using namespace std;
+
+class SchaltwerkElement
+{
+
+
+
+
+	private:
+
+		string name;		
+		double laufzeitEinzelgatter;
+		int anzahlNachfolger;
+		bool isEingangsElement;
+		bool isAusgangsElement;
+		short anzahlEingangssignale;
+		SchaltwerkElement(GatterTyp gTyp);
+		~SchaltwerkElement();
+
+		GatterTyp* typ;
+		SchaltwerkElement* nachfolgerElemente;
+
+
+
+	public:
+
+		string getName() ;
+		GatterTyp getTyp();
+		double getLaufzeitEinzelgatter();
+		SchaltwerkElement getNachfolger(int pos);
+		int getAnzahlNachfolger();
+		short getAnzahlEingangssignale();
+		bool getIsEingangsElement();
+		bool getIsAusgagsElement();
+		void setName(string n);
+		void nachfolgerHinzufügen(SchaltwerkElement schaltwerkElement, int pos);
+		void setAnzahlNachfolger(int anzahlN);
+		void setAnzahlEingangssignale(short anzahlE);
+		void setIsEingangsElement(bool isEingangsEl);
+		void setIsAusgangsElement(bool isAusgangsEl);
+		void setLaufzeitEinzelgatter(double lfz);
+};
